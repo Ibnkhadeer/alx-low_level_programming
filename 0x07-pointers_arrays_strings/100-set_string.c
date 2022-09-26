20216416f45e89e2d99c6d4cc7c3e23eb2933e16
Abbas Halliru Abdullahi <abbasumamaki22@gmail.com>
2022-09-26 13:41:01 -0700

    Task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,18 +1,28 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code
+ * print_diagsums - print the sum of the two
+ *                 diagonals of a square matrix
+ *                 of integers.
+ * @a: input pointer
+ * @size: size of the matrix
  *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s0 = "Bob Dylan";
-    char *s1 = "Robert Allen";
+ * Return: nothing
+*/
 
-    printf("%s, %s\n", s0, s1);
-    set_string(&s1, s0);
-    printf("%s, %s\n", s0, s1);
-    return (0);
+void print_diagsums(int *a, int size)
+{
+	int i, S1, S2;
+
+	S1 = 0;
+	S2 = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+			S1 += a[i];
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			S2 += a[i];
+	}
+	printf("%d, %d\n", S1, S2);
 }
