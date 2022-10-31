92c7bb18004886aecc47e3b7d933d7bd86d488c1
Ibnkhadeer <89810440+Ibnkhadeer@users.noreply.github.com>
2022-11-01 00:41:44 +0100

    Update 2-append_text_to_file.c
    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,50 @@
 #include "main.h"
 
 /**
- *  * append_text_to_file - a funcion that appends text at the end
- *   *                      of a file
- *    *
- *     * @filename: file to add data to
- *      * @text_content: text content to add to file
- *       *
- *        * Return: 1 on success, -1 on failure
- *        */
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+* append_text_to_file - appends text at the end of a file.
+* @filename: file to append.
+* @text_content: info to append into the file.
+* Return: 1 on success, -1 on failure
+*/
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-		int file, app_status, words = 0;
+	ssize_t nletters;
+	int file;
 
-			if (filename == NULL) /*check if file is present*/
-						return (-1);
-
-				/*open file, with append option with write rights*/
-				file = open(filename, O_APPEND | O_WRONLY);
-					if (file == -1) /*check if file is present*/
-								return (-1);
-
-						if (text_content) /*append content to file if its not NULL*/
-								{
-											while (text_content[words] != '\0') /*find number of words*/
-															words++;
-
-													/*append to file*/
-													app_status = write(file, text_content, words);
-															if (app_status == -1) /*check if append was a success*/
-																			return (-1);
-																}
-
-							close(file); /*close file*/
-								return (1);
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
